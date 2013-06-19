34897345e10810593025744219348510bf80291d
Michael Bishop <michael@michaelsplace.net>
2013-06-18 20:48:54 -0700

    icon font override
    overrides the image fonts in bootstrap to allow icon fonts to be used

--- package.js
+++ package.js
@@ -9,7 +9,10 @@ Package.on_use(function (api){
 	api.add_files('lib/font-awesome/font/fontawesome-webfont.woff', 'client');
 	api.add_files('lib/font-awesome/font/FontAwesome.otf', 'client');
 	api.add_files('lib/font-awesome/css/font-awesome.css', 'client');
-	
+
 	//font path override
 	api.add_files('path-override.css', 'client');
+
+	//icon override for icon fonts like Font Awesome
+	api.add_files('icon-override.css', 'client');
 });
